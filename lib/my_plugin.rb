7073c3873ceb045a81a9ddc41ef7ee5067cebfc5
aykut.yurtmen <aykut.yurtmen@ulakhaberlesme.com.tr>
2023-06-26 22:15:49 +0000

    head etiketine JS ve CSS yükleyerek issue altında sekme yaratmak

--- lib/my_plugin.rb
+++ lib/my_plugin.rb
@@ -1,56 +1,14 @@
 module MyPlugin
   module Hooks
+
     class ViewIssuesShowDetailsBottomHook < Redmine::Hook::ViewListener
-      include Redmine::I18n
-      render_on(
-        :view_issues_show_details_bottom,
-        :partial => 'hooks/issues/view_issues_form_details_bottom',
-        :layout => false
-      )
+
+      def view_layouts_base_html_head(context = {})
+        tags = javascript_include_tag('test_results.js', :plugin => 'my_plugin') + stylesheet_link_tag("test_results.css", :plugin => "my_plugin", :media => "all")
+        tags.html_safe
+      end
+
     end
-    class ViewIssuesShowSidebarBottomHook < Redmine::Hook::ViewListener
-      include Redmine::I18n
-      render_on(
-        :view_issues_show_sidebar_bottom,
-        :partial => 'hooks/issues/view_issues_show_sidebar_bottom',
-        :layout => false
-      )
-    end
-    
-    class ViewIssuesShowDescriptionBottomHook < Redmine::Hook::ViewListener
-      include Redmine::I18n
-      render_on(
-        :view_issues_show_description_bottom,
-        :partial => 'hooks/issues/view_issues_show_description_bottom',
-        :layout => false
-      )
-    end
-    
-    class ViewIssuesContextMenuEndHook < Redmine::Hook::ViewListener
-      include Redmine::I18n
-      render_on(
-        :view_issues_context_menu_end,
-        :partial => 'hooks/issues/view_issues_context_menu_end',
-        :layout => false
-      )
-    end
-    
-    class ViewLayoutsBaseSidebarHook < Redmine::Hook::ViewListener
-      include Redmine::I18n
-      render_on(
-        :view_layouts_base_sidebar,
-        :partial => 'hooks/layouts/view_layouts_base_sidebar',
-        :layout => false
-      )
-    end
-    
-    class ViewProjectsShowSidebarBottomHook < Redmine::Hook::ViewListener
-      include Redmine::I18n
-      render_on(
-        :view_projects_show_sidebar_bottom,
-        :partial => 'hooks/projects/view_projects_show_sidebar_bottom',
-        :layout => false
-      )
-    end
+
   end
 end
