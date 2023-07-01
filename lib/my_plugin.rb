8db50106b39f13781625366018a22de223d724c3
root <root@gitlab.novalocal>
2023-07-01 08:46:56 +0000

    code complexity optimization

--- lib/my_plugin.rb
+++ lib/my_plugin.rb
@@ -2,30 +2,36 @@ module MyPlugin
   module Hooks
     class OylesineBirSinif < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context = {})
+        controller = context[:request].parameters[:controller]
+        action = context[:request].parameters[:action]
+        Rails.logger.info(">>> MyPlugin.OylesineBirSinif.view_layouts_base_html_head >>> controller: #{controller}>>> action: #{action}")
+
+        return nil unless controller == "issues" && action == "show"
+
         issue_id = context[:request].params[:id]
+        return nil unless issue_id.present? && issue_id.to_i.to_s == issue_id
 
-        if issue_id.present? && issue_id.to_i.to_s == issue_id
-          Rails.logger.info(">>>> issue_id 1: #{issue_id}")
-          issue_data = get_issue(issue_id)
-          tags = javascript_include_tag(
-            "test_results.js",
-            :plugin => "my_plugin",
-          ) + stylesheet_link_tag(
-            "test_results.css",
-            :plugin => "my_plugin",
-            :media => "all",
-          )
-          # Diğer JavaScript kodunu ekleyelim
-          additional_js = javascript_tag(
-            %Q(
-              // Burada başka JavaScript kodları olabilir
-              console.log('Additional JavaScript code');
-              var issueData = #{issue_data};
-            )
-          )
+        # Eğer sayfa "issues" sayfası ise devam edelim, aksi halde hiçbir içerik eklemeyelim
+        Rails.logger.info(">>>> issue_id 1: #{issue_id}")
+        issue_data = get_issue(issue_id)
+        tags = javascript_include_tag(
+          "test_results.js",
+          :plugin => "my_plugin",
+        ) + stylesheet_link_tag(
+          "test_results.css",
+          :plugin => "my_plugin",
+          :media => "all",
+        )
+        # Diğer JavaScript kodunu ekleyelim
+        additional_js = javascript_tag(
+          %Q(
+                // Burada başka JavaScript kodları olabilir
+                console.log('Additional JavaScript code');
+                var issueData = #{issue_data};
+              )
+        )
 
-          tags.html_safe + additional_js
-        end
+        tags.html_safe + additional_js
       end
 
       private
